c490757155ac8340adf3a7d4291ec12d424a4ac0
ilukyanchik <ivan.lukyanchik@idt.net>
2020-07-04 12:20:50 +0300

    Proper exception handling added

--- src/main/java/by/kolesa/backend/exception/GlobalControllerAdvice.java
+++ src/main/java/by/kolesa/backend/exception/GlobalControllerAdvice.java
@@ -1,44 +1,48 @@
 package by.kolesa.backend.exception;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 @RestController
 @RequiredArgsConstructor
 public class GlobalControllerAdvice {
 
-    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
-    public String handleBadRequestException() {
-        return "It is a bad request!";
+    @ExceptionHandler({HttpClientErrorException.BadRequest.class, CustomBadRequest.class, MethodArgumentTypeMismatchException.class})
+    public ResponseEntity<String> handleBadRequestException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public String handleUserNotFoundException() {
-        return "User Not Found";
+    public ResponseEntity<String> handleUserNotFoundException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(SendMailException.class)
-    public String handleSendMailException() {
-        return "Exception occurred while sending mail";
-    }
-
-    @ExceptionHandler(LoadingKeystoreException.class)
-    public String handleLoadingKeystoreException() {
-        return "Exception occurred while loading keystore";
+    @ExceptionHandler({SendMailException.class, LoadingKeystoreException.class})
+    public ResponseEntity<String> handleSendMailException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(InvalidTokenException.class)
-    public String handleInvalidTokenException() {
-        return "Token is invalid";
+    public ResponseEntity<String> handleInvalidTokenException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
-    @ExceptionHandler(CustomBadRequest.class)
-    public String handleCustomBadRequest(CustomBadRequest ex) {
-        return ex.getMessage();
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentialsException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAll(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
